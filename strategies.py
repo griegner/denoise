be8c43dce10608522f00f7f90ef749bc15355358
griegner <gabriel.riegner@gmail.com>
2021-05-18 14:35:59 -0700

    update strategies

--- strategies.py
+++ strategies.py
@@ -4,24 +4,27 @@ strategies = {
     'HMPCompCorScrub': {'strategy': ['high_pass', 'motion', 'compcor', 'scrub'], 
                          'motion': 'basic', 
                          'compcor': 'anat', 
-                         'n_compcor': 5, 
-                         'acompcor_combined': True, 
+                         'n_compcor': 'auto', 
+                         'acompcor_combined': True,
+                         'scrub': 'basic', 
                          'fd_thresh': 0.9, 
-                         'std_dvars_thresh': None},
+                         'std_dvars_thresh': 5},
 
     'HMPCompCorScrubGS': {'strategy': ['high_pass', 'motion', 'compcor', 'scrub', 'global'], 
                          'motion': 'basic', 
                          'compcor': 'anat', 
-                         'n_compcor': 5, 
+                         'n_compcor': 'auto', 
                          'acompcor_combined': True, 
+                         'scrub': 'basic',
                          'fd_thresh': 0.9, 
-                         'std_dvars_thresh': None, 
+                         'std_dvars_thresh': 5, 
                          'global_signal': 'basic'},               
 
     'HMPWMCSFScrub': {'strategy': ['high_pass', 'motion', 'wm_csf', 'scrub'], 
                          'motion': 'basic', 
                          'wm_csf': 'full',
+                         'scrub': 'basic',
                          'fd_thresh': 0.9, 
-                         'std_dvars_thresh': None}
+                         'std_dvars_thresh': 5}
     }
 
