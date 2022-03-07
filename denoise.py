d90bfcbd5d15e1d10944f4cbfc334bb26ade8658
griegner <johndoe@example.com>
2022-03-07 08:31:43 -0800

    format code

--- denoise.py
+++ denoise.py
@@ -1,189 +1,269 @@
-
 from strategies import strategies
 import report
 
-import warnings; warnings.filterwarnings('ignore')
+import warnings
+
+warnings.filterwarnings("ignore")
 from load_confounds import Confounds
 from bids.layout import parse_file_entities
 from pathlib import Path
 import argparse
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set_style('white')
-import numpy as np; import pandas as pd
+import seaborn as sns
+
+sns.set_style("white")
+import numpy as np
+import pandas as pd
 from nilearn import plotting, datasets, image
 from nilearn.input_data import NiftiLabelsMasker
 from nilearn.connectome import ConnectivityMeasure
 
+
 def get_args(strategies):
-    parser = argparse.ArgumentParser(description='denoise fMRI data')
-    parser.add_argument('fmriprep', type=Path, help='path to fmriprep directory')
-    parser.add_argument('strategy', type=str, help=' - '.join(strategies))
-    parser.add_argument('--smooth_fwhm', type=float, default=6, help='smoothing kernel, default fwhm 6mm')
-    parser.add_argument('--report_only', action='store_true', help='run group-level summary only')
+    parser = argparse.ArgumentParser(description="denoise fMRI data")
+    parser.add_argument("fmriprep", type=Path, help="path to fmriprep directory")
+    parser.add_argument("strategy", type=str, help=" - ".join(strategies))
+    parser.add_argument(
+        "--smooth_fwhm",
+        type=float,
+        default=6,
+        help="smoothing kernel, default fwhm 6mm",
+    )
+    parser.add_argument(
+        "--report_only", action="store_true", help="run group-level summary only"
+    )
     args = parser.parse_args()
-    assert args.strategy in strategies, f'{args.strategy} is not a valid strategy'
-    assert args.fmriprep.is_dir(), 'fmriprep directory does not exist'
+    assert args.strategy in strategies, f"{args.strategy} is not a valid strategy"
+    assert args.fmriprep.is_dir(), "fmriprep directory does not exist"
     return args
+
 
 class Data:
     def __init__(self, fmriprep, strategy):
-        print('\nindexing files... ', end='')
+        print("\nindexing files... ", end="")
         self.fmriprep = fmriprep
         self.masks = self.get_masks()
         self.preprocs = self.get_preprocs()
         self.confounds = self.get_confounds(strategy)
         self.motion_dfs = self.get_motion_dfs()
-        assert len(self.masks)==len(self.preprocs)==len(self.confounds)==len(self.motion_dfs), \
-            'missings fmriprep files'
-        print(f'found {len(self.preprocs)} images')
+        assert (
+            len(self.masks)
+            == len(self.preprocs)
+            == len(self.confounds)
+            == len(self.motion_dfs)
+        ), "missings fmriprep files"
+        print(f"found {len(self.preprocs)} images")
 
     def get_masks(self):
-        masks =  self.fmriprep.glob('**/sub-*_task-n*_space-MNI152NLin6Asym_res-2_desc-brain_mask.nii.gz')
+        masks = self.fmriprep.glob(
+            "**/sub-*_task-n*_space-MNI152NLin6Asym_res-2_desc-brain_mask.nii.gz"
+        )
         return sorted([str(mask) for mask in masks])
 
     def get_preprocs(self):
-        preprocs = self.fmriprep.glob('**/sub-*_task-n*_space-MNI152NLin6Asym_res-2_desc-preproc_bold.nii.gz')
+        preprocs = self.fmriprep.glob(
+            "**/sub-*_task-n*_space-MNI152NLin6Asym_res-2_desc-preproc_bold.nii.gz"
+        )
         return sorted([str(preproc) for preproc in preprocs])
 
     def get_confounds(self, strategy):
         return Confounds(**strategy).load(self.preprocs)
 
     def get_motion_dfs(self):
-        dfs = sorted(self.fmriprep.glob('**/sub-*_task-n*_desc-confounds_timeseries.tsv'))
-        return [pd.read_csv(df, sep='\t', usecols=['framewise_displacement', 'std_dvars', 'rmsd']) for df in dfs]
+        dfs = sorted(
+            self.fmriprep.glob("**/sub-*_task-n*_desc-confounds_timeseries.tsv")
+        )
+        return [
+            pd.read_csv(
+                df, sep="\t", usecols=["framewise_displacement", "std_dvars", "rmsd"]
+            )
+            for df in dfs
+        ]
+
 
 def build_path(derivatives, sub, task, strategy):
-    path = derivatives / f'denoise/sub-{sub}'
+    path = derivatives / f"denoise/sub-{sub}"
     path.mkdir(parents=True, exist_ok=True)
     # BIDS pattern: sub-{subject}_task-{task}_strat-{strategy}_{suffix}.{extension}
-    path_plot = path / f'sub-{sub}_task-{task}_strat-{strategy}_plot.png'
-    path_none = path / f'sub-{sub}_task-{task}_strat-none_vect.npy'
-    path_vector = path / f'sub-{sub}_task-{task}_strat-{strategy}_vect.npy'
-    path_qc = path / f'sub-{sub}_task-{task}_qc.tsv'
+    path_plot = path / f"sub-{sub}_task-{task}_strat-{strategy}_plot.png"
+    path_none = path / f"sub-{sub}_task-{task}_strat-none_vect.npy"
+    path_vector = path / f"sub-{sub}_task-{task}_strat-{strategy}_vect.npy"
+    path_qc = path / f"sub-{sub}_task-{task}_qc.tsv"
     return path_plot, path_none, path_vector, path_qc
+
 
 def get_timeseries(preproc, atlas, mask, smooth_fwhm, derivatives):
     masker = NiftiLabelsMasker(
-                atlas, 
-                mask_img=mask, 
-                smoothing_fwhm=smooth_fwhm, 
-                strategy='mean',
-                memory=str(derivatives / '.denoise_cache'), 
-                memory_level=3
-                )
+        atlas,
+        mask_img=mask,
+        smoothing_fwhm=smooth_fwhm,
+        strategy="mean",
+        memory=str(derivatives / ".denoise_cache"),
+        memory_level=3,
+    )
     return masker.fit_transform(preproc[0]), masker.fit_transform(preproc[1])
 
+
 def get_correlations(timeseries, path_none, path_vector, strategy):
-    correlation_vector = ConnectivityMeasure(kind='correlation', vectorize=True, discard_diagonal=True)
+    correlation_vector = ConnectivityMeasure(
+        kind="correlation", vectorize=True, discard_diagonal=True
+    )
     vector = correlation_vector.fit_transform(timeseries)
-    np.save(path_none, vector[0]) # noise
-    np.save(path_vector, vector[1]) # denoise
-    correlation_matrix = ConnectivityMeasure(kind='correlation', vectorize=False)
+    np.save(path_none, vector[0])  # noise
+    np.save(path_vector, vector[1])  # denoise
+    correlation_matrix = ConnectivityMeasure(kind="correlation", vectorize=False)
     matrix = correlation_matrix.fit_transform(timeseries)
-    np.fill_diagonal(matrix[0], np.nan); np.fill_diagonal(matrix[1], np.nan)
+    np.fill_diagonal(matrix[0], np.nan)
+    np.fill_diagonal(matrix[1], np.nan)
     return vector, matrix
 
+
 def plot_dist(ax, vector):
-    sns.kdeplot(vector[0], color='#B62E33', ax=ax)
-    sns.kdeplot(vector[1], color='#3C83BC', ax=ax)
-    ax.axvline(x=0, c='k', alpha=.3, linestyle='dashed')
+    sns.kdeplot(vector[0], color="#B62E33", ax=ax)
+    sns.kdeplot(vector[1], color="#3C83BC", ax=ax)
+    ax.axvline(x=0, c="k", alpha=0.3, linestyle="dashed")
+
 
 def plot_connectome(ax, matrix, sub, task):
-    ax.set_title(f'sub-{sub} task-{task}', loc='left', weight='bold', size='medium')
-    plotting.plot_matrix(matrix[0], colorbar=False, tri='full', axes=ax)
-    plotting.plot_matrix(matrix[1], colorbar=False, tri='lower', axes=ax)
+    ax.set_title(f"sub-{sub} task-{task}", loc="left", weight="bold", size="medium")
+    plotting.plot_matrix(matrix[0], colorbar=False, tri="full", axes=ax)
+    plotting.plot_matrix(matrix[1], colorbar=False, tri="lower", axes=ax)
+
 
 def plot_outliers(ax, motion_df, fd_thresh, dvars_thresh):
-    
-    motion_df['fd_outliers'] = motion_df.framewise_displacement.gt(fd_thresh) 
-    motion_df['dvars_outliers'] = motion_df.std_dvars.gt(dvars_thresh)
-    motion_df['outliers'] = motion_df.fd_outliers + motion_df.dvars_outliers
+
+    motion_df["fd_outliers"] = motion_df.framewise_displacement.gt(fd_thresh)
+    motion_df["dvars_outliers"] = motion_df.std_dvars.gt(dvars_thresh)
+    motion_df["outliers"] = motion_df.fd_outliers + motion_df.dvars_outliers
     outliers = motion_df.index[motion_df.outliers].to_list()
     rows = motion_df.shape[0]
-    p_outliers = len(outliers)/rows
-    
+    p_outliers = len(outliers) / rows
+
     ax[0].set_title(
-        f'FD mean={motion_df.framewise_displacement.mean():.2f} \
+        f"FD mean={motion_df.framewise_displacement.mean():.2f} \
         DVARS mean={motion_df.std_dvars.mean():.2f} \
-        outliers={p_outliers:.2%}', 
-        loc='left', 
-        size='small'
-        )
+        outliers={p_outliers:.2%}",
+        loc="left",
+        size="small",
+    )
 
-    ax[0].axhline(y=fd_thresh, color='r', linestyle=':', lw=.6)
-    ax[0].plot(motion_df.framewise_displacement, color='k', lw=0.5)
+    ax[0].axhline(y=fd_thresh, color="r", linestyle=":", lw=0.6)
+    ax[0].plot(motion_df.framewise_displacement, color="k", lw=0.5)
 
-    ax[1].axhline(y=dvars_thresh, color='r', linestyle=':', lw=.6)
-    ax[1].plot(motion_df.std_dvars, color='k', lw=0.5)
-    ax[1].plot(outliers, [0]*len(outliers), color='r', linestyle='none', marker='|')
+    ax[1].axhline(y=dvars_thresh, color="r", linestyle=":", lw=0.6)
+    ax[1].plot(motion_df.std_dvars, color="k", lw=0.5)
+    ax[1].plot(outliers, [0] * len(outliers), color="r", linestyle="none", marker="|")
 
-    return p_outliers>0.1 or motion_df.framewise_displacement.mean()>0.5 # exclusion criteria
+    return (
+        p_outliers > 0.1 or motion_df.framewise_displacement.mean() > 0.5
+    )  # exclusion criteria
+
 
 def plot_carpet(ax, preproc, mask):
     plotting.plot_carpet(preproc[0], mask_img=mask, axes=ax[0])
     plotting.plot_carpet(preproc[1], mask_img=mask, axes=ax[1])
 
-def run_summary(vector, matrix, sub, task, motion_df, fd_thresh, dvars_thresh, preproc, mask, paths):
-    fig, axs = plt.subplots(6, figsize=(4,9), gridspec_kw={'height_ratios': [2, 10, 1, 1, 2, 2]})
-    for ax in axs: ax.axis('off'); ax.margins(0,.02)
+
+def run_summary(
+    vector, matrix, sub, task, motion_df, fd_thresh, dvars_thresh, preproc, mask, paths
+):
+    fig, axs = plt.subplots(
+        6, figsize=(4, 9), gridspec_kw={"height_ratios": [2, 10, 1, 1, 2, 2]}
+    )
+    for ax in axs:
+        ax.axis("off")
+        ax.margins(0, 0.02)
     plot_dist(axs[0], vector)
     plot_connectome(axs[1], matrix, sub, task)
     exclude = plot_outliers(axs[2:4], motion_df, fd_thresh, dvars_thresh)
     plot_carpet(axs[4:], preproc, mask)
-    if exclude: 
-        axs[0].set_title('exclude', color='r', fontweight='bold', loc='left')
+    if exclude:
+        axs[0].set_title("exclude", color="r", fontweight="bold", loc="left")
         [path.unlink() for path in paths[1:]]
-    fig.savefig(paths[0], dpi=300, bbox_inches='tight')
+    fig.savefig(paths[0], dpi=300, bbox_inches="tight")
     plt.close(fig)
 
+
 def group_summary(derivatives, strategy, atlas):
-    print(f'group-level summary: strat-{strategy}_report.html')
-    denoise = derivatives / 'denoise'
+    print(f"group-level summary: strat-{strategy}_report.html")
+    denoise = derivatives / "denoise"
     qcs, vectors_noise, vectors_denoise, plots = report.find_files(denoise, strategy)
     qc_means = report.qc_means(qcs)
-    fig, axs = plt.subplots(ncols=4, figsize=(20,2), gridspec_kw={'width_ratios': [1,2,2,2]})
-    for ax in axs: ax.axis('off'); ax.margins(0,0)
+    fig, axs = plt.subplots(
+        ncols=4, figsize=(20, 2), gridspec_kw={"width_ratios": [1, 2, 2, 2]}
+    )
+    for ax in axs:
+        ax.axis("off")
+        ax.margins(0, 0)
     report.plot_atlas(atlas, axs[0])
     report.plot_summary_dist(axs[1], vectors_noise, vectors_denoise, denoise, strategy)
-    report.compare(denoise, qc_means['rmsd'], axs[2:])
-    fig.savefig(f'{denoise}/group/strat-{strategy}_plot.png', dpi=300)
+    report.compare(denoise, qc_means["rmsd"], axs[2:])
+    fig.savefig(f"{denoise}/group/strat-{strategy}_plot.png", dpi=300)
     report.html_report(denoise, strategy, strategies[strategy], plots)
     plt.close(fig)
     del fig, axs
 
 
-def main():  
+def main():
 
     args = get_args(strategies.keys())
     derivatives = args.fmriprep.parent
-    fd_thresh = strategies[args.strategy]['fd_thresh'] 
-    dvars_thresh = strategies[args.strategy]['std_dvars_thresh'] 
-    atlas = datasets.fetch_atlas_basc_multiscale_2015(version='asym')['scale122']
-    
+    fd_thresh = strategies[args.strategy]["fd_thresh"]
+    dvars_thresh = strategies[args.strategy]["std_dvars_thresh"]
+    atlas = datasets.fetch_atlas_basc_multiscale_2015(version="asym")["scale122"]
+
     if not args.report_only:
 
         data = Data(args.fmriprep, strategies[args.strategy])
 
-        for mask, preproc, confound, motion_df in zip(data.masks, data.preprocs, data.confounds, data.motion_dfs):
+        for mask, preproc, confound, motion_df in zip(
+            data.masks, data.preprocs, data.confounds, data.motion_dfs
+        ):
 
             file_entities = parse_file_entities(preproc)
-            sub, task= file_entities['subject'], file_entities['task']
-            print(f'> sub-{sub} task-{task}')
-            path_plot, path_none, path_vector, path_qc = build_path(derivatives, sub, task, args.strategy)
-            motion_df.to_csv(path_qc, sep='\t', index=False)
+            sub, task = file_entities["subject"], file_entities["task"]
+            print(f"> sub-{sub} task-{task}")
+            path_plot, path_none, path_vector, path_qc = build_path(
+                derivatives, sub, task, args.strategy
+            )
+            motion_df.to_csv(path_qc, sep="\t", index=False)
 
             mask, preproc_noise = image.load_img(mask), image.load_img(preproc)
-            assert mask.shape == preproc_noise.shape[:-1], 'mask and preproc images have different shapes'
-            preproc_denoise = image.clean_img(preproc, detrend=False, standardize=False, confounds=confound, mask_img=mask)
+            assert (
+                mask.shape == preproc_noise.shape[:-1]
+            ), "mask and preproc images have different shapes"
+            preproc_denoise = image.clean_img(
+                preproc,
+                detrend=False,
+                standardize=False,
+                confounds=confound,
+                mask_img=mask,
+            )
             preproc = preproc_noise, preproc_denoise
 
-            timeseries = get_timeseries(preproc, atlas, mask, args.smooth_fwhm, derivatives)
+            timeseries = get_timeseries(
+                preproc, atlas, mask, args.smooth_fwhm, derivatives
+            )
 
-            vector, matrix = get_correlations(timeseries, path_none, path_vector, args.strategy)
+            vector, matrix = get_correlations(
+                timeseries, path_none, path_vector, args.strategy
+            )
 
-            run_summary(vector, matrix, sub, task, motion_df, fd_thresh, dvars_thresh, preproc, mask, (path_plot, path_none, path_vector, path_qc))
-
+            run_summary(
+                vector,
+                matrix,
+                sub,
+                task,
+                motion_df,
+                fd_thresh,
+                dvars_thresh,
+                preproc,
+                mask,
+                (path_plot, path_none, path_vector, path_qc),
+            )
 
     group_summary(derivatives, args.strategy, atlas)
 
-if __name__ == '__main__': main()
+
+if __name__ == "__main__":
+    main()
--- report.py
+++ report.py
@@ -1,36 +1,52 @@
-import warnings; warnings.filterwarnings('ignore')
+import warnings
+
+warnings.filterwarnings("ignore")
 
 import re
 import numpy as np
 import pandas as pd
 from nilearn import plotting
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set_style('white')
+import seaborn as sns
+
+sns.set_style("white")
 from scipy import stats
 from statsmodels.stats.multitest import fdrcorrection
 
+
 def find_files(denoise, strategy):
-    denoise.joinpath('group').mkdir(exist_ok=True)
-    qcs = sorted(denoise.glob(f'sub-*/*_qc.tsv'))
-    vectors_noise = sorted(denoise.glob(f'sub-*/*strat-none_vect.npy'))
-    vectors_denoise = sorted(denoise.glob(f'sub-*/*strat-{strategy}_vect.npy'))
-    plots = sorted(denoise.glob(f'sub-*/*strat-{strategy}_plot.png'))
+    denoise.joinpath("group").mkdir(exist_ok=True)
+    qcs = sorted(denoise.glob("sub-*/*_qc.tsv"))
+    vectors_noise = sorted(denoise.glob("sub-*/*strat-none_vect.npy"))
+    vectors_denoise = sorted(denoise.glob(f"sub-*/*strat-{strategy}_vect.npy"))
+    plots = sorted(denoise.glob(f"sub-*/*strat-{strategy}_plot.png"))
     return qcs, vectors_noise, vectors_denoise, plots
 
+
 def qc_means(qcs):
-    columns = pd.read_csv(qcs[0], sep='\t').columns
-    qc_means = [pd.read_csv(qc, sep='\t').mean(axis=0).to_list() for qc in qcs]
+    columns = pd.read_csv(qcs[0], sep="\t").columns
+    qc_means = [pd.read_csv(qc, sep="\t").mean(axis=0).to_list() for qc in qcs]
     return pd.DataFrame(qc_means, columns=columns)
 
+
 def plot_atlas(atlas, ax):
-    plotting.plot_roi(atlas, display_mode='xz', cut_coords=(0,0) , annotate=False, draw_cross=False, axes=ax)
+    plotting.plot_roi(
+        atlas,
+        display_mode="xz",
+        cut_coords=(0, 0),
+        annotate=False,
+        draw_cross=False,
+        axes=ax,
+    )
+
 
 def plot_summary_dist(ax, vectors_noise, vectors_denoise, denoise, strategy):
 
     edges = []
     [edges.extend(np.load(vector)) for vector in vectors_noise]
-    sns.kdeplot(edges, color='#B62E33', linewidth=1, ax=ax)
-    np.save(denoise/f'group/strat-none_vect.npy', edges); del edges
+    sns.kdeplot(edges, color="#B62E33", linewidth=1, ax=ax)
+    np.save(denoise / "group/strat-none_vect.npy", edges)
+    del edges
 
     edges = []
     means = []
@@ -38,14 +54,29 @@ def plot_summary_dist(ax, vectors_noise, vectors_denoise, denoise, strategy):
         vector = np.load(vector)
         edges.extend(vector)
         means.append(np.mean(vector))
-        sns.kdeplot(vector, color='#3C83BC', linewidth=.08, ax=ax)
-    np.save(denoise/f'group/strat-{strategy}_vect.npy', edges); del edges
+        sns.kdeplot(vector, color="#3C83BC", linewidth=0.08, ax=ax)
+    np.save(denoise / f"group/strat-{strategy}_vect.npy", edges)
+    del edges
 
-    ax.axvline(x=0, c='k', alpha=.3, linestyle='dashed')
-    ax.plot(means, [-0.1]*len(means), color='#3C83BC', linestyle='none', marker='|', markersize=10, alpha=.2)
+    ax.axvline(x=0, c="k", alpha=0.3, linestyle="dashed")
+    ax.plot(
+        means,
+        [-0.1] * len(means),
+        color="#3C83BC",
+        linestyle="none",
+        marker="|",
+        markersize=10,
+        alpha=0.2,
+    )
 
-    ax.text(-0.1, 0.9, strategy, transform=ax.transAxes, weight='bold')
-    ax.text(-0.1, -0.1, f'distribution of connectivity values (r) across {len(means)} functional runs', transform=ax.transAxes)
+    ax.text(-0.1, 0.9, strategy, transform=ax.transAxes, weight="bold")
+    ax.text(
+        -0.1,
+        -0.1,
+        f"distribution of connectivity values (r) across {len(means)} functional runs",
+        transform=ax.transAxes,
+    )
+
 
 def qc_fc(qc, npys, strategies):
 
@@ -53,56 +84,89 @@ def qc_fc(qc, npys, strategies):
     for npy, strategy in zip(npys, strategies):
         fc = np.load(npy).reshape(len(qc), -1)
         pvals_unc = [stats.pearsonr(qc, fc_col)[1] for fc_col in fc.T]
-        pvals_corr = fdrcorrection(pvals_unc, alpha=.05)[0]
+        pvals_corr = fdrcorrection(pvals_unc, alpha=0.05)[0]
         qc_fc_[strategy] = (pvals_corr.sum() / len(pvals_corr)) * 100
 
     return pd.Series(qc_fc_)
 
+
 def compare(denoise, qc_means, ax):
-    npys = sorted(denoise.glob('group/*vect.npy'))
-    strategies = [re.search('strat-(.*)_', npy.stem).group(1) for npy in npys]
+    npys = sorted(denoise.glob("group/*vect.npy"))
+    strategies = [re.search("strat-(.*)_", npy.stem).group(1) for npy in npys]
 
     # axis 0
     means = {}
-    cmap = plt.get_cmap('binary')
+    cmap = plt.get_cmap("binary")
     for npy, strategy in zip(npys, strategies):
         dist = np.load(npy)
         dist_mean = dist.mean()
         means[strategy] = dist_mean
-        color = cmap(1-dist_mean)
-        if strategy == 'none': color='#B62E33'
-        sns.kdeplot(dist, linewidth=0.2, ax=ax[0], color=color, fill=True, alpha=.2, label=strategy)
+        color = cmap(1 - dist_mean)
+        if strategy == "none":
+            color = "#B62E33"
+        sns.kdeplot(
+            dist,
+            linewidth=0.2,
+            ax=ax[0],
+            color=color,
+            fill=True,
+            alpha=0.2,
+            label=strategy,
+        )
 
-    means = sorted(means.items(), key=lambda x:x[1])
+    means = sorted(means.items(), key=lambda x: x[1])
     h, l = ax[0].get_legend_handles_labels()
     handels = [h[l.index(mean[0])] for mean in means]
-    labels = [f'{idx+1}. {mean[0]}' for idx, mean in enumerate(means)]
-    
-    ax[0].legend(handels, labels, prop={'size': 6}, loc='upper left', bbox_to_anchor=(-0.1,.85))
-    ax[0].axvline(x=0, c='k', alpha=.3, linestyle='dashed')
-    ax[0].text(-0.1, 0.9, 'strategies (sorted by mean)', transform=ax[0].transAxes)
-    ax[0].text(-0.1, -0.1, f'distribution of connectivity values (r) across {len(strategies)} strategies', transform=ax[0].transAxes)
+    labels = [f"{idx+1}. {mean[0]}" for idx, mean in enumerate(means)]
+
+    ax[0].legend(
+        handels, labels, prop={"size": 6}, loc="upper left", bbox_to_anchor=(-0.1, 0.85)
+    )
+    ax[0].axvline(x=0, c="k", alpha=0.3, linestyle="dashed")
+    ax[0].text(-0.1, 0.9, "strategies (sorted by mean)", transform=ax[0].transAxes)
+    ax[0].text(
+        -0.1,
+        -0.1,
+        f"distribution of connectivity values (r) across {len(strategies)} strategies",
+        transform=ax[0].transAxes,
+    )
 
     # axis 1
     qc_fc_ = qc_fc(qc_means, npys, strategies).sort_values(ascending=False)
-    colors = ['#B62E33' if strat == 'none' else 'k' for strat in qc_fc_.index]
-    qc_fc_.plot(kind='barh', color=colors, alpha=.2, ax=ax[1])
+    colors = ["#B62E33" if strat == "none" else "k" for strat in qc_fc_.index]
+    qc_fc_.plot(kind="barh", color=colors, alpha=0.2, ax=ax[1])
     sns.despine(ax=ax[1], left=True, bottom=True)
-    ax[1].bar_label(ax[1].containers[0], fontsize=6, fmt='   %d%%')
+    ax[1].bar_label(ax[1].containers[0], fontsize=6, fmt="   %d%%")
 
     order = qc_fc_.index
     for idx, strat in enumerate(order):
-        ax[1].text(0.5, idx, f'{len(order)-idx}. {strat}', fontsize=6, ha='left', va='center')
+        ax[1].text(
+            0.5, idx, f"{len(order)-idx}. {strat}", fontsize=6, ha="left", va="center"
+        )
 
-    ax[1].text(-0.1, -0.1, f'qc-fc: % of edges correlated with RMS motion (q < .05)', transform=ax[1].transAxes)
+    ax[1].text(
+        -0.1,
+        -0.1,
+        "qc-fc: % of edges correlated with RMS motion (q < .05)",
+        transform=ax[1].transAxes,
+    )
 
-   
+
 def html_report(denoise, strategy, confounds, plots):
 
-    with open(f'{denoise}/group/strat-{strategy}_report.html', 'w') as f_out:
-        print(f"<img src='{denoise}/group/strat-{strategy}_plot.png' style='float: left; width: 100%'>", '<hr>', sep='\n', file=f_out)
+    with open(f"{denoise}/group/strat-{strategy}_report.html", "w") as f_out:
+        print(
+            f"<img src='{denoise}/group/strat-{strategy}_plot.png' style='float: left; width: 100%'>",
+            "<hr>",
+            sep="\n",
+            file=f_out,
+        )
         print(f"<small>{confounds}</small>", file=f_out)
-        
+
         for idx, plot in enumerate(plots):
-            if idx%4==0: print('<hr>', file=f_out)
-            print(f"<img src='{str(plot)}' style='float: left; width: 24%; margin-right: 1%'>", file=f_out)
+            if idx % 4 == 0:
+                print("<hr>", file=f_out)
+            print(
+                f"<img src='{str(plot)}' style='float: left; width: 24%; margin-right: 1%'>",
+                file=f_out,
+            )
